bd999baa1c131849cb140aa980592f7ddc97c26a
PRATUSH KUMAR GOUDA <pratush.gouda@gmail.com>
2024-10-17 20:59:43 +0530

    initated socket.io  for real time communication with the chat bot

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 const SpeechRecognition = window.SpeechRecognition ||
 window.webitSpeechRecognition;
 const recognition = new SpeechRecognition();
+const socket =io();
 
 recognition.lang = 'en-US';
 recognition.interimResults = false;
@@ -10,8 +11,9 @@ document.querySelector('button').addEventListener('click',()=>{
 
 })
 recognition.addEventListener('result',(e)=>{
+    socket.emit('chat message', text);
     let last = e.results.length - 1;
     let text = e.results[last][0].trasnscript;
     console.log('Confidence :'+e.results[0][0].confidence);
-    
+
 })
